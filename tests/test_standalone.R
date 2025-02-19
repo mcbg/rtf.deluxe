d8c65d32d0b70829fc83744cc06aeb389b3e2fad
Michael Galanakis <michael.galanakis@omicron.dk>
2025-02-19 15:54:36 +0100

    adds test with header with categories

--- tests/test_standalone.R
+++ tests/test_standalone.R
@@ -1,4 +1,12 @@
 devtools::load_all()
 
+# 1
 create_standalone_rtf_file(iris) |>
   cat(file='test.rtf', sep='\n')
+
+# 2
+new_iris = iris
+names(new_iris) = names(iris) |> paste(c('sepal', 'sepal', 'petal', 'petal', ''), sep = '\t')
+create_standalone_rtf_file(new_iris) |>
+  cat(file='test2.rtf', sep='\n')
+
