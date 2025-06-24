6bbb9b58e8ec34f231eaa35df9bc66436540d32b
Michael Galanakis <michael.galanakis@omicron.dk>
2025-06-24 14:21:59 +0200

    issue where rtf_create_link was messed up

--- R/rtf_code_creation.R
+++ R/rtf_code_creation.R
@@ -17,7 +17,8 @@ rtf_create_header = \(text) {
   paste0(text_rtf)
 }
 
-rtf_create_link = \(text, reference, page_number, fixed_width = 100) {
+rtf_create_link = \(text, reference, page_number) {
+  sprintf('{\\pard {\\field{\\*\\fldinst{HYPERLINK "#%s"}}{\\fldrslt{\\tldot\\tqr %s %s \\tab %d\\par}}}}',
     reference, get_tx_value(), text, page_number)
 }
 
