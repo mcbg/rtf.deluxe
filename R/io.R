755ed33fc0ec2d5957d41668262b6262b0471c99
Michael Galanakis <michael.galanakis@omicron.dk>
2025-09-08 15:57:58 +0200

    adds documentation

--- R/io.R
+++ R/io.R
@@ -28,7 +28,38 @@ replace_with_nonbreaking_spaces = \(dataset) {
 
 # metadata ----------------------------------------------------------------
 
+#' Save output and metadata
+#'
+#' This functions saves the output and the metadata
+#' as a JSON-file. Tables and listings are saved as RDS-files, whereas
+#' figures are saved as PNG-files. A footnote is added with the time, date and program name
+#' for traceability.
+#'
+#' @param tfl_output output can either be a data.frame, data.table, or a list of data.frames
+#' , where each entry will be shown on it's own page. For figures provide a ggplot object.
+#' @param tfl_path the path to the folder where files will be saved
+#' @param name Name of the output. This will also be the file name.
+#' @param number Appendix section number of output. For example 14.2.1 for a efficacy table.
+#' @param title The title of the output. This is used for the header and the table of contents.
+#' @param subtitle Optional subtitle
+#' @param footnotes a character vector, where each entry will be shown on a separate line.
+#' @param type 'table', 'listing', or 'figure'
+#' @param program_name the name of the program that generated the output.
+#' @param nonbreaking_space a bool indicating if normal spaces should be replaced with nonbreaking spaces when calculating the column width of tables.
 #' @export
+#' @examples
+#' save_output_and_metadata(
+#'   tfl_output=mtcars,
+#'   tfl_path='myoutput',
+#'   name='cars',
+#'   number='1.1',
+#'   title='Cars',
+#'   subtitle='',
+#'   footnotes='Source: 1974 Motor Trend US magazine',
+#'   type='listing',
+#'   program_name='example.r'
+#' )
+#'
 save_output_and_metadata = \(
   tfl_output,
   tfl_path,
@@ -42,7 +73,6 @@ save_output_and_metadata = \(
   program_name,
   nonbreaking_space=(type == 'table')
 ) {
-
 
   # save table
   if ('list' %in% class(tfl_output)) {
@@ -101,6 +131,11 @@ save_output_and_metadata = \(
   )
 }
 
+#' Read all JSON metadata in folder
+#'
+#' @param tfl_path path that contains JSON metadata files
+#' @returns A list where each entry contains the metadata of a output.
+#' The metadata is stored as a list.
 #' @export
 read_metadata = \(tfl_path) {
   tfl_path |> list.files(pattern = '*.json', full.names = TRUE) |>
@@ -138,6 +173,14 @@ save_outputs_by_list = \(output_list, metadata_function) {
 
 # RTF i/o -----------------------------------------------------------------
 
+#' Creates RTF-file with output
+#'
+#' @param metadata metadata object that you get from `read_metadata`.
+#' @param metadata_path the path to the RDS files and PNG files of the output
+#' @param output_filename path of the resulting RTF-file
+#' @param document_title the title of the document on the front page
+#' @param title_page_info A data.frame with a `field` and `value` columns. This information is shown on title page.
+#' @param header_rtf Raw RTF-code that can be added to header
 #' @export
 write_rtf = \(metadata, metadata_path, output_filename, document_title, title_page_info, header_rtf='') {
   rtf_code = metadata |>
